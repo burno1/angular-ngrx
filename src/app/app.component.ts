d68e696df9f4a93c47120350bc8a5e563ecb55f0
Bruno Fernandes <brunofernandes100@hotmail.com.br>
2022-09-28 12:18:05 -0300

    Added state for service calling and selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {addBook, removeBook, retrievedBookList} from "./book-list/state/actions/book.actions";
+import {selectBookCollection, selectBooks} from "./book-list/state/selectors/books.selector";
+import {GoogleBooksService} from "./book-list/books-service.service";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ngrx';
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  onAdd(bookId: string) {
+    this.store.dispatch(addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(removeBook({ bookId }));
+  }
+
+  constructor(
+    private booksService: GoogleBooksService,
+    private store: Store
+  ) {}
+
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) => this.store.dispatch(retrievedBookList({ books })));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter/reducers/counter.reducer';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CounterComponent} from './counter/counter.component';
+import {StoreModule} from '@ngrx/store';
+import {counterReducer} from './counter/reducers/counter.reducer';
+import {BookListComponent} from './book-list/book-list.component';
+import {booksReducer} from "./book-list/state/reducers/book.reducer";
+import {collectionReducer} from "./book-list/state/reducers/collection.reducer";
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+import {HttpClientModule} from "@angular/common/http";
+
+let reducers = {count: counterReducer, books: booksReducer, collection: collectionReducer};
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    BookListComponent,
+    BookCollectionComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({count:counterReducer})
+    StoreModule.forRoot(reducers)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
